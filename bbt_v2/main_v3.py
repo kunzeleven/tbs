094912a3bb29119cb60ca5ce327a420612df9270
breakout <kunzeleven@gmail.com>
2025-07-01 12:54:38 +0700

    Update main_v3.py

--- bbt_v2/main_v3.py
+++ bbt_v2/main_v3.py
@@ -354,9 +354,8 @@ def booking_form_page():
     st.markdown("")
     col1, col2, col3 = st.columns([1, 1, 1])
     with col2:
-        st.markdown(''':blue[*please use light mode in browser to get better experience*]''') 
-        #st.markdown(''':gray[--please use light mode theme in web browser to get better experience--]''') 
-        st.markdown(''':blue[*for adding new meeting room please contact rania traction*]''')
+        st.markdown(''':gray[*please use light mode in browser to get better experience*]''') 
+        st.markdown(''':gray[*for adding new meeting room please contact rania traction*]''')
 
 def debug_supabase_connection():
     """Fungsi untuk debugging koneksi Supabase"""
